379ed4e9af5b40bfa7acab66fcf692598d5b766e
Luiz Fumes <lcfumes@gmail.com>
2019-07-20 01:36:16 -0300

    Create user function and model

---
+++ functions/user.js
@@ -0,0 +1,20 @@
+const users = require('../models/user')
+
+module.exports.handler = async (event) => {
+  const user = JSON.parse(event.body)
+  try {
+    const insert = await users.save(user)
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        ...user
+      })
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 500
+    }
+  }
+}
